a41164eab7ee3051ab5102688cf82f1ceafd5ea7
kijima-riku <riku.bulls.0116@icloud.com>
2025-02-21 15:42:37 +0900

    define repository layer

---
+++ src/main/java/com/pokereco/pokereco/repository/TokenRepository.java
@@ -0,0 +1,12 @@
+package com.pokereco.pokereco.repository;
+
+import com.pokereco.pokereco.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByAccessToken(String accessToken);
+    Optional<Token> findByRefreshToken(String refreshToken);
+
+}
