aed2b2ffd07528a58b4d46094ec78cbe2f23aa30
kijima-riku <riku.bulls.0116@icloud.com>
2025-02-23 16:20:11 +0900

    changed DB schema

--- src/main/resources/db/migration/V5__Create_favorite_decks_table.sql
+++ src/main/resources/db/migration/V5__Create_favorite_decks_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS favorite_decks (
     id         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id    INT NOT NULL,
+    user_id    INT NOT NULL UNIQUE,
     deck_id    INT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
