d2465e472af2a0497d53d2e3216129ac6c73d3b0
kijima-riku <riku.bulls.0116@icloud.com>
2025-03-19 16:17:39 +0900

    use response builder in deck

--- src/main/java/com/pokereco/pokereco/controller/DeckController.java
+++ src/main/java/com/pokereco/pokereco/controller/DeckController.java
@@ -2,7 +2,9 @@ package com.pokereco.pokereco.controller;
 
 import com.pokereco.pokereco.dto.DeckDto;
 import com.pokereco.pokereco.dto.DeckRequestDto;
+import com.pokereco.pokereco.responseBuilder.ResponseBuilder;
 import com.pokereco.pokereco.service.DeckService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,18 +13,22 @@ import java.util.List;
 @RequestMapping("/api/v1/deck")
 public class DeckController {
   private final DeckService deckService;
+  private final ResponseBuilder responseBuilder;
 
-  public DeckController(final DeckService deckService) {
+  public DeckController(final DeckService deckService, final ResponseBuilder responseBuilder) {
     this.deckService = deckService;
+    this.responseBuilder = responseBuilder;
   }
 
   @GetMapping
-  public List<DeckDto> getDeckList() {
-    return deckService.getAllDecks();
+  public ResponseEntity<?> getDeckList() {
+    final List<DeckDto> deckList = deckService.getAllDecks();
+    return responseBuilder.buildSuccessResponse(deckList);
   }
 
   @PostMapping
-  public DeckDto createDeck(@RequestBody DeckRequestDto request) {
-    return deckService.createDeck(request);
+  public ResponseEntity<?> createDeck(@RequestBody DeckRequestDto request) {
+    final DeckDto createdDeck = deckService.createDeck(request);
+    return responseBuilder.buildSuccessResponse(createdDeck);
   }
 }
