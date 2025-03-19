332eb099b4b7f4c84cb37f84efc2acc777bf6aed
kijima-riku <riku.bulls.0116@icloud.com>
2025-03-19 15:28:32 +0900

    create response builder

---
+++ src/main/java/com/pokereco/pokereco/responseBuilder/ResponseBuilder.java
@@ -0,0 +1,28 @@
+package com.pokereco.pokereco.responseBuilder;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+@Component
+public class ResponseBuilder {
+
+  // 成功レスポンス
+  public <T> ResponseEntity<Map<String, Object>> buildSuccessResponse(T data) {
+    Map<String, Object> responseBody = new LinkedHashMap<>();
+    responseBody.put("data", data);
+
+    return new ResponseEntity<>(responseBody, HttpStatus.OK);
+  }
+
+  // エラーレスポンス
+  public ResponseEntity<Map<String, Object>> buildErrorResponse(String message, HttpStatus status) {
+    Map<String, Object> responseBody = new LinkedHashMap<>();
+    responseBody.put("data", message);
+
+    return new ResponseEntity<>(responseBody, status);
+  }
+}
